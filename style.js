0c92cbba5255a3a6360f42943f0187ea4dc4a630
Partho Biswas <parthobiswas6969@gmail.com>
2025-07-11 19:15:57 -0400

    log in / sign up page / simple home page

---
+++ style.js
@@ -0,0 +1,59 @@
+
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+
+signupBtn.onclick = () => {
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+};
+
+loginBtn.onclick = () => {
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+};
+
+signupLink.onclick = () => {
+  signupBtn.click();
+  return false;
+};
+
+// Handle Sign Up
+document.querySelector("form.signup").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = this.querySelector("input[placeholder='Email Address']").value;
+  const password = this.querySelector("input[placeholder='Password']").value;
+  const confirmPassword = this.querySelector("input[placeholder='Confirm Password']").value;
+
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+    return;
+  }
+
+  const users = JSON.parse(localStorage.getItem("users") || "{}");
+  if (users[email]) {
+    alert("User already exists!");
+    return;
+  }
+
+  users[email] = password;
+  localStorage.setItem("users", JSON.stringify(users));
+  window.location.href = "home.html";
+});
+
+// Handle Login
+document.querySelector("form.login").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = this.querySelector("input[placeholder='Email Address']").value;
+  const password = this.querySelector("input[placeholder='Password']").value;
+
+  const users = JSON.parse(localStorage.getItem("users") || "{}");
+
+  if (users[email] && users[email] === password) {
+    window.location.href = "home.html";
+  } else {
+    alert("Invalid email or password.");
+  }
+});
